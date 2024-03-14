58263f49f75a98783cd53522fd9f601b8b6f251c
Gitolli16 <157802994+Gitolli16@users.noreply.github.com>
2024-03-14 13:59:22 +0200

    Delete esp32-wroomtest directory

--- esp32-wroomtest/.pio/build/upesy_wroom/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/student/.platformio/packages/framework-espidf/components/mbedtls/mbedtls/library/error.c
--- esp32-wroomtest/.pio/build/upesy_wroom/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/home/student/.platformio/packages/framework-espidf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
