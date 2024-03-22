2cece0ec639bcb5159052a000ad54d03bc2efca5
ollihaap <olli.haapala@edu.turkuamk.fi>
2024-03-22 14:00:59 +0200

    lab5_c

--- esp32s2_lab5_c/src/main.c
+++ esp32s2_lab5_c/src/main.c
@@ -10,14 +10,6 @@
 #define INPUT_PIN 15
 #define LED_PIN 3
 
-int state = 0;
-QueueHandle_t interputQueue;
-
-static void IRAM_ATTR gpio_interrupt_handler(void *args)
-{  
-    int pinNumber = (int)args;
-    xQueueSendFromISR(interputQueue, &pinNumber, NULL);
-}
 
 void LED_Control_Task(void *params)
 {
@@ -42,34 +34,67 @@ void LED_Control_Task(void *params)
         gpio_set_level(ledList[i], 1);
     }
 
-    int pinNumber2, count, state = 0;
+    int state = 0;
+    int flag = 0;
 
-    while (true)
-    {   
-        for (int j = 0; j < 10; j++) {
+    while (1) {
+        if (state % 2 == 0) {
             for (int k = 0; k < 7; k++)
             {
-                gpio_set_level(valot[j][k], 0);
-
-            }
-            if (xQueueReceive(interputQueue, &pinNumber2, 0)) { 
-                printf("GPIO %d was pressed %d times. The state is %d\n", pinNumber2, count++, gpio_get_level(INPUT_PIN));
-                state++;
-                while (!gpio_get_level(pinNumber2))
+                gpio_set_level(valot[9][k], 0);
+                printf("state 0\n");
+                while (gpio_get_level(INPUT_PIN) == 0)
                 {
-                    for (int l = 0; l < 7; l++)
+                    printf("state 0 while\n");
+                    if (gpio_get_level(INPUT_PIN) == 1)
                     {
-                        gpio_set_level(valot[9][l], 0);
+                        state++;
+                        flag++;
+                        printf("state 0 while-if %d\n", state);
+                        break;
+
                     }
                 }
-            }
-            usleep(1000000);
-            for (int k = 0; k < 7; k++)
-            {
-                gpio_set_level(valot[j][k], 1);
+                if (flag > 0) {
+                    for (int k = 0; k < 7; k++)
+                    {
+                        gpio_set_level(valot[9][k], 0);
+                    }
+                    flag = 0;
+                    break;
+                }
             }
         }
-    }
+        if (state % 2 == 1) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 7; k++) {
+                    gpio_set_level(valot[j][k], 0);
+                    printf("state 1\n");
+                }
+                while (gpio_get_level(INPUT_PIN) == 0)
+                {
+                    printf("state 1 while\n");
+                    if (gpio_get_level(INPUT_PIN) == 1)
+                    {
+                        state++;
+                        printf("state 1 while-if %d\n", state);
+                        flag++;
+                        break;
+                    }
+                }
+                usleep(500000);
+                for (int k = 0; k < 7; k++)
+                {
+                    gpio_set_level(valot[j][k], 1);
+                }
+                if (flag > 0) {
+                    flag = 0;
+                    break;
+                }
+       
+            }
+            }
+        }
 }
 
 void app_main()
@@ -79,13 +104,13 @@ void app_main()
 
     gpio_pad_select_gpio(INPUT_PIN);
     gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
-    gpio_pulldown_en(INPUT_PIN);
-    gpio_pullup_dis(INPUT_PIN);
-    gpio_set_intr_type(INPUT_PIN, GPIO_INTR_POSEDGE);
 
-    interputQueue = xQueueCreate(1, sizeof(int));
+    
     xTaskCreate(LED_Control_Task, "LED_Control_Task", 2048, NULL, 1, NULL);
 
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(INPUT_PIN, gpio_interrupt_handler, (void *)INPUT_PIN);
 }
+
+
+
+
+    
